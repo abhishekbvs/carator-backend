b9f70c494209c44fc556713fb8aba662cff5fd7b
Abhishek Bvs <bvsabhishek@gmail.com>
2019-07-15 22:02:24 +0530

    Changed urls

--- carator/urls.py
+++ carator/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import include, path
 from django.contrib import admin
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('admin/', admin.site.urls),
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
